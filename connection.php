b933698b779d0c1ceb9cc5786386ad247ca8f0e8
musicprogram <juan-o-henao@hotmail.es>
2018-04-22 09:29:58 -0500

    realizando conexión con la base de datos php_connection creada en phpmyadmin

---
+++ connection.php
@@ -0,0 +1,14 @@
+<?php 
+	$dsn = 'mysql:dbname=php_connection;host=127.0.0.1';
+	$user = 'root';
+	$password = '';
+
+	try{
+		$pdo = new PDO($dsn,
+									 $user,
+									 $password
+									);
+	}catch( PDOException $e){
+		echo 'Error al contactarnos' .$e->getMessage();
+	}	
+?>
